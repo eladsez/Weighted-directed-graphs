3d34475a7a5071d336d2cfac23e34dc8d97220d6
Nerya Bigon <nerya0001@gmail.com>
2021-12-01 14:16:09 +0200

    more comments added

--- src/logicControl/DWGraphAlgo.java
+++ src/logicControl/DWGraphAlgo.java
@@ -3,22 +3,45 @@ package logicControl;
 import api.DirectedWeightedGraph;
 import api.NodeData;
 import org.json.simple.parser.ParseException;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * This class represents a Directed (positive) Weighted Graph Theory Algorithms including:
+ * 0. clone(); (copy)
+ * 1. init(graph);
+ * 2. isConnected(); // strongly (all ordered pais connected)
+ * 3. double shortestPathDist(int src, int dest);
+ * 4. List<NodeData> shortestPath(int src, int dest);
+ * 5. NodeData center(); // finds the NodeData which minimizes the max distance to all the other nodes.
+ * // Assuming the graph isConnected, elese return null.
+ * 6. List<NodeData> tsp(List<NodeData> cities); // computes a list of consecutive nodes which go over all the nodes in cities.
+ * <p>
+ * 7. save(file); // JSON file
+ * 8. load(file); // JSON file
+ */
+
 public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
-
+    /**
+     * graph -> the graph
+     * time -> Discovery time for dfs
+     */
     private DirectedWeightedGraph graph;
-    private int time; //Discovery time for dfs
+    private int time;
 
+    /**
+     * Inits the graph on which this set of algorithms operates on.
+     *
+     * @param g
+     */
     @Override
     public void init(DirectedWeightedGraph g) {
-        if (g == null){
+        if (g == null) {
             this.graph = new DWGraph();
-        }
-        else this.graph = g;
+        } else this.graph = g;
     }
 
     @Override
@@ -45,12 +68,22 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         return returnGraph;
     }
 
+    /**
+     * Returns true if and only if (iff) there is a valid path from each node to each
+     * other node. NOTE: assume directional graph (all n*(n-1) ordered pairs).
+     *
+     * @return
+     */
     @Override
     public boolean isConnected() {
         int i = tarjan().size();
         return i == 1;
     }
 
+    /**
+     * the algorithm we used in the isConncted function
+     * this is the driving function, and the heavy lifting is done with the strong Connect function
+     */
     private HashMap tarjan() {
         HashMap<Integer, HashMap<Integer, Node>> components = new HashMap<>();
         this.time = 0;
@@ -66,6 +99,9 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         return components;
     }
 
+    /**
+     * this is the heavy function of the isConnect function
+     */
     private void strongConnect(Node node, Stack<Node> stack, HashMap components) {
         node.setRevealTime(this.time);
         node.setLowLink(this.time);
@@ -97,6 +133,15 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         }
     }
 
+    /**
+     * Computes the length of the shortest path between src to dest
+     * Note: if no such path --> returns -1
+     * we used the dijkstra algorithm
+     *
+     * @param src  - start node
+     * @param dest - end (target) node
+     * @return the shortest dist
+     */
     @Override
     public double shortestPathDist(int src, int dest) {
         HashMap<Integer, Double> dist = new HashMap<>();
@@ -128,6 +173,16 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         return dist.get(dest);
     }
 
+    /**
+     * Computes the the shortest path between src to dest - as an ordered List of nodes:
+     * src--> n1-->n2-->...dest
+     * Note if no such path --> returns null;
+     * we used the dijkstra algorithm
+     *
+     * @param src  - start node
+     * @param dest - end (target) node
+     * @return
+     */
     @Override
     public List<NodeData> shortestPath(int src, int dest) {
         HashMap<Integer, Double> dist = new HashMap<>();
@@ -172,6 +227,13 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         return returnList;
     }
 
+
+    /**
+     * Finds the NodeData which minimizes the max distance to all the other nodes.
+     * Assuming the graph isConnected, elese return null. See: https://en.wikipedia.org/wiki/Graph_center
+     *
+     * @return the Node data to which the max shortest path to all the other nodes is minimized.
+     */
     @Override
     public NodeData center() {
         Node center = null;
@@ -190,6 +252,9 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         return center;
     }
 
+    /**
+     * helper function to the center function
+     */
     private double farestDist(int src) {
         Iterator iter = this.graph.nodeIter();
         Node checkNode;
@@ -206,11 +271,25 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         return returnDist;
     }
 
+
+    /**
+     * Computes a list of consecutive nodes which go over all the nodes in cities.
+     * the sum of the weights of all the consecutive (pairs) of nodes (directed) is the "cost" of the solution -
+     * the lower the better.
+     */
     @Override
     public List<NodeData> tsp(List<NodeData> cities) {
         return null;
     }
 
+
+    /**
+     * Saves this weighted (directed) graph to the given
+     * file name - in JSON format
+     *
+     * @param file - the file name (may include a relative path).
+     * @return true - iff the file was successfully saved
+     */
     @Override
     public boolean save(String file) {
         try {
@@ -226,6 +305,16 @@ public class DWGraphAlgo implements api.DirectedWeightedGraphAlgorithms {
         return true;
     }
 
+
+    /**
+     * This method loads a graph to this graph algorithm.
+     * if the file was successfully loaded - the underlying graph
+     * of this class will be changed (to the loaded one), in case the
+     * graph was not loaded the original graph should remain "as is".
+     *
+     * @param file - file name of JSON file
+     * @return true - iff the graph was successfully loaded.
+     */
     @Override
     public boolean load(String file) {
         try {
