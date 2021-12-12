00c7dfcebb62781b0ee22fefe39da3db9eb93785
Nerya Bigon <nerya0001@gmail.com>
2021-12-12 09:15:40 +0200

    TSP bug fixed

--- tests/logicControl/DWGraphAlgoTest.java
+++ tests/logicControl/DWGraphAlgoTest.java
@@ -20,7 +20,11 @@ class DWGraphAlgoTest {
     @BeforeEach
     void init() {
         this.algo = new DWGraphAlgo();
-        this.algo.load("Data/G3.json");
+        this.algo.load("Data/G1.json");
+//        this.algo.load("Data/G2.json");
+//        this.algo.load("Data/G3.json");
+//        this.algo.load("Data/1000Nodes.json");
+//        this.algo.load("Data/10000Nodes.json");
 
     }
 
@@ -64,13 +68,11 @@ class DWGraphAlgoTest {
 
     @Test
     void center() {
-        this.algo.load("Data/1000Nodes.json");
-//        Node center = (Node) this.algo.center();
+        this.algo.center();
     }
 
     @Test
     void tsp() {
-        this.algo.load("Data/1000Nodes.json");
         List<NodeData> tspTR = new LinkedList<>();
         NodeData n1 = new Node((Node) algo.getGraph().getNode(1));
         NodeData n2 = new Node((Node) algo.getGraph().getNode(2));
@@ -79,9 +81,9 @@ class DWGraphAlgoTest {
         NodeData n5 = new Node((Node) algo.getGraph().getNode(5));
         NodeData n6 = new Node((Node) algo.getGraph().getNode(6));
         NodeData n7 = new Node((Node) algo.getGraph().getNode(7));
-//        NodeData n8 = new Node((Node) algo.getGraph().getNode(8));
-//        NodeData n9 = new Node((Node) algo.getGraph().getNode(9));
-//        NodeData n10 = new Node((Node) algo.getGraph().getNode(10));
+        NodeData n8 = new Node((Node) algo.getGraph().getNode(8));
+        NodeData n9 = new Node((Node) algo.getGraph().getNode(9));
+        NodeData n10 = new Node((Node) algo.getGraph().getNode(10));
 //        NodeData n11 = new Node((Node) algo.getGraph().getNode(11));
 //        NodeData n12 = new Node((Node) algo.getGraph().getNode(12));
 //        NodeData n13 = new Node((Node) algo.getGraph().getNode(13));
@@ -94,9 +96,9 @@ class DWGraphAlgoTest {
         tspTR.add(n5);
         tspTR.add(n6);
         tspTR.add(n7);
-//        tspTR.add(n8);
-//        tspTR.add(n9);
-//        tspTR.add(n10);
+        tspTR.add(n8);
+        tspTR.add(n9);
+        tspTR.add(n10);
 //        tspTR.add(n11);
 //        tspTR.add(n12);
 //        tspTR.add(n13);
@@ -104,5 +106,4 @@ class DWGraphAlgoTest {
 //        tspTR.add(n15);
         List<NodeData> ans = algo.tsp(tspTR);
     }
-
 }
